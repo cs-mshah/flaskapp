8aee0aa6552b2fe2c7ee76932ee96e1fbdb26b76
cs-mshah <mt-shah@hotmail.com>
2020-07-09 12:56:06 +0530

    jinja templates

--- firstapp.py
+++ firstapp.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from flask import render_template
+import feedparser
 
-app = Flask(__name__) # create instance of Flask
+app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'hello world'
+RSS_FEEDS={
+    'toi': "https://timesofindia.indiatimes.com/rssfeedstopstories.cms",
+    'bbc': "http://feeds.bbci.co.uk/news/rss.xml",
+    'fox': "http://feeds.foxnews.com/foxnews/latest"
+}
 
-if __name__ == '__main__': # if this is run directly without import in another 
-    app.run(port=5000,debug=True)
+@app.route("/")
+@app.route("/<publication>")
+def get_news(publication='toi'):
+
+    feed = feedparser.parse(RSS_FEEDS[publication])
+
+    return render_template("home.html",articles=feed['entries'])
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
